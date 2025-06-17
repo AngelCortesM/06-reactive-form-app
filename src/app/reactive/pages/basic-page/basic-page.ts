5327ba48a0cb45b7a1d87b5f4d1d80eba718fb27
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-06-17 15:44:08 -0500

    feat(basic-page): implement reactive form with validation and error handling

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,10 +1,63 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.html',
   styleUrl: './basic-page.css',
 })
-export class BasicPage {}
+export class BasicPage {
+  private readonly fb = inject(FormBuilder);
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  isValidField(fieldName: string): boolean | null {
+    return (
+      !!this.myForm.controls[fieldName].errors &&
+      this.myForm.controls[fieldName].touched
+    );
+  }
+
+  getFieldError(fieldName: string): string | null {
+    if (!this.myForm.controls[fieldName].errors) return null;
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es obligatorio';
+        case 'minlength':
+          return `La longitud mínima es ${errors['minlength'].requiredLength} caracteres.`;
+        case 'min':
+          return `El valor mínimo es ${errors['min'].min}`;
+      }
+    }
+    return null;
+  }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log('Form submitted successfully!', this.myForm.value);
+    this.myForm.reset();
+  }
+
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+}
