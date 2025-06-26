9fd11150b405d24f1ed22839830207666056b441
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-06-26 14:44:40 -0500

    feat(switches-page): implement reactive form with validation for gender and terms acceptance

--- src/app/reactive/pages/switches-page/switches-page.ts
+++ src/app/reactive/pages/switches-page/switches-page.ts
@@ -1,10 +1,30 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.html',
   styleUrl: './switches-page.css',
 })
-export class SwitchesPage {}
+export class SwitchesPage {
+  formUtils = FormUtils;
+  private readonly fb = inject(FormBuilder);
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true],
+    termAndConditions: [false, Validators.requiredTrue],
+  });
+
+  submit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+  }
+}
